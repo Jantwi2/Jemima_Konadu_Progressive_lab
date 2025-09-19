6b51147f3f32c53dfeb5241a3b2e6cb1fda17841
Jemima Konadu Antwi <124621185+Jantwi2@users.noreply.github.com>
2025-09-19 17:04:14 +0000

    Add files via upload

--- controllers/customer_controller.php
+++ controllers/customer_controller.php
@@ -14,6 +14,11 @@ function add_customer_ctr($customer_name,$customer_email,$customer_contact,$cust
 	return $adduser->add_customer($customer_name,$customer_email,$customer_contact,$customer_password,$customer_country,$customer_city, $customer_role);
 }
 
+function login_customer_ctr($customer_email, $customer_password) {
+	$loginUser = new customer_class();
+	return $loginUser->login_customer($customer_email, $customer_password);
+}
+
 //--INSERT--//
 
 //--SELECT--//
